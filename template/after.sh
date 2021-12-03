84cdb7f94eeabf3274646b29cbfde46e12cc89ba
Nguyen Marc <nguyen_marc@live.fr>
2021-12-03 18:45:48 +0100

    fix(script): don't use j

--- template/after.sh
+++ template/after.sh
@@ -1,7 +1,7 @@
 # Wait for the Jupyter server to start
 echo "Waiting for Jupyter server to open port ${port}..."
 echo "TIMING - Starting wait at: $(date)"
-if wait_until_port_used "${host}:${port}" 3600; then
+if wait_until_port_used "${host}:${port}" 1800; then
   echo "Discovered Jupyter server listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
 else
